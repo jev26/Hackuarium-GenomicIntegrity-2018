48143c37e7e58c223559bc772e94b72412801e52
veillard <jennifer.veillard@epfl.ch>
2018-10-02 12:34:53 +0200

    Make it works for Win

--- MovingStage/tif2jpg-folder.py
+++ MovingStage/tif2jpg-folder.py
@@ -14,18 +14,20 @@ os.makedirs(newpath)
 for image in os.listdir(file):
 
     infile = image
-    outfile = file + "-jpg/" + infile[:-3]+"jpg"
-    infile = file + "/" + image
 
-    img = plt.imread(infile)
+    if infile != '.DS_Store':
+        outfile = file + "-jpg/" + infile[:-3]+"jpg"
+        infile = file + "/" + image
 
-    #plt.figure(frameon=False)
-    plt.gray()
-    plt.axis('off')
-    plt.imshow(img)
+        img = plt.imread(infile)
 
-    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        #plt.figure(frameon=False)
+        plt.gray()
+        plt.axis('off')
+        plt.imshow(img)
 
-    #plt.show()
-    plt.savefig(outfile)#, transparent = True, bbox_inches='tight', pad_inches=-0.1)
-    #plt.savefig(outfile, bbox_inches = 'tight', transparent=True, pad_inches=0)#, "JPEG", quality=100)
+        plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+
+        #plt.show()
+        plt.savefig(outfile)#, transparent = True, bbox_inches='tight', pad_inches=-0.1)
+        #plt.savefig(outfile, bbox_inches = 'tight', transparent=True, pad_inches=0)#, "JPEG", quality=100)
